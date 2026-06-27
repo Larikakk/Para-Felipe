43515eca74d96efd45ce728c90df9a65fabc6d87
Larikakk <larissastm2019@gmail.com>
2026-06-27 09:37:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-<script>
-  function abrirCarta() {
-    document.getElementById("carta").innerText =
-      "Espero que você goste."}
-</script>
+function iniciarCoracoes() {
+  const coracoes = document.getElementById("coracoes");
+  if (coracoes) {
+    coracoes.classList.remove("ativo");
+    void coracoes.offsetWidth;
+    coracoes.classList.add("ativo");
+  }
+}
